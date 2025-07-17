65df3c479b1dbdb2f6bbf6e4ac80ee92e941281d
Raj <karkeemiraj@gmail.com>
2025-07-17 14:57:12 +0545

    feat: Create Industry model and migration for industries table

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Industry;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
         public function industry(){
-            return view('admin.industry.index');
+            $industries = Industry::all();
+            return view('admin.industry.index', compact('industries'));
+          
         }
+        public function industryAdd(){
+            if(request()->isMethod('post')){
+                $industryData = new Industry();
+                $industryData->icon = request('icon');
+                $industryData->title = request('title');
+                $industryData->description = request('description');
+                $industryData->services = json_encode(request('services', []));
+                $industryData->save();
+            }
+            
+            return view('admin.industry.add');
+        }
+
 }
